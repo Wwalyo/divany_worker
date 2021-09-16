23195da561cbdb33910505ecf16bf895628986ac
Alena <alenalesk@gmail.com>
2021-09-16 14:55:58 +0300

    forwardMessage

--- index.js
+++ index.js
@@ -10,29 +10,8 @@ bot.command('quit', (ctx) => {
   ctx.leaveChat()
 })
 
-bot.on('text', (ctx) => {
-  // Explicit usage
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-  // Using context shortcut
-  ctx.reply(`Hello ${ctx.state.role}`)
-})
-
-bot.on('callback_query', (ctx) => {
-  // Explicit usage
-  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
-
-  // Using context shortcut
-  ctx.answerCbQuery()
-})
-
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
-
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
+bot.on('text', ctx => {
+  ctx.forwardMessage("-1001591252809");
 })
 
 bot.launch()
